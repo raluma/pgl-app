933bd6327337d642592f51495316d8dc74db9a49
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-12-03 14:06:10 +0000

    trying examples screen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text } from 'react-native';
+
+export default function App() {
+    return (
+        <Text>
+            Estas en la Home Screen
+        </Text>
+    );
+}
