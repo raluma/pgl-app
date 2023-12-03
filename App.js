230aea96db498c686eeff93b0ed6106a361df1e3
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-12-03 14:05:47 +0000

    creating navigation (first draft)

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './src/screens/HomeScreen';
+import FavScreen from './src/screens/FavScreen';
+import { Icon } from '@rneui/themed';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused }) => {
+            return <Icon name={ 
+              route.name === 'Home' ? 'home' 
+              : 'heart'} type='font-awesome' color={
+                focused && route.name === 'Home' ? 'blue' 
+                : focused && route.name === 'Fav' ? 'red'
+                : 'black'
+              } />;
+          },
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'black',
+        })}
+      >
+        <Tab.Screen 
+          name='Home' 
+          children={()=> <HomeScreen />}
+        />
+
+        <Tab.Screen 
+          name='Fav' 
+          children={()=> <FavScreen />}
+        />
+
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
