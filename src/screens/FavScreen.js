d9d2dbffeb6d073a1c77b40d65588ca74f7d6152
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-12-13 12:35:21 +0000

    trying zustand state manager

--- src/screens/FavScreen.js
+++ src/screens/FavScreen.js
@@ -1,10 +1,14 @@
-import * as React from 'react';
-import { Text } from 'react-native';
+import React from 'react';
+import { useSessionStore } from '../services/sessions';
+import Login from '../components/Login';
 
 export default function App() {
+    const session = useSessionStore(state => state.session); 
+    console.log(session);
+
     return (
-        <Text>
-            Estas en la Fav Screen
-        </Text>
+        <>
+            <Login />
+        </>
     );
 }
