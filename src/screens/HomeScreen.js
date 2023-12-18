c5c27b4f669a24f53eeb2c4828bed5550b6ae503
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-12-18 19:34:21 +0000

    creating home Screen (first draft)

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,38 @@
-import * as React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, View, Image, Text } from 'react-native';
 
-export default function App() {
+const idsHomeList = [];
+
+for (let i = 1; i <= 9; i++) {
+    idsHomeList.push(i);
+}
+
+export default function HomeScreen() {
+    const width = 120;
+    const height = 120;
+    
     return (
-        <Text>
-            Estas en la Home Screen
-        </Text>
+        <View style={styles.container}>
+            {
+                idsHomeList.map(id => {
+                    return (
+                        <Image 
+                            key={id}
+                            source = {{ uri: `https://img.foxes.cool/scary/${id}.jpg?width=${width}&height=${height}` }}  
+                            style = {{ width, height }} 
+                        />
+                    )
+                })
+            }
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        gap: 4
+    }
+});
