6493b7d1322c0b759c9f1d691a1d4f0f56a3d9f1
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-12-19 13:02:08 +0000

    renaming and adding signup component

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,17 +1,18 @@
 import Profile from '../components/Profile';
 import Login from '../components/Login';
+import Signup from '../components/Signup';
 
 export default function Account({ session }) {
     return (
         <>
             {
-                session.state.nombre === "Inicio" && session.state.value ?
+                session.state.name === "Login" && session.state.value ?
                     <Profile session={session} />
-                : 
+                : session.state.name === "Signup" ?
+                    <Signup session={session} />
+                :
                     <Login session={session} />
             }
-
-
         </>
     );
 }
