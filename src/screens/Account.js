e1eeef962563c373bb014b8896e3813cdb63cbb7
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-12-20 11:30:56 +0000

    adding status messages

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,17 +1,41 @@
 import Profile from '../components/Profile';
 import Login from '../components/Login';
 import Signup from '../components/Signup';
+import { Alert } from 'react-native';
+import { useSessionStore } from '../services/sessions';
 
 export default function Account({ session }) {
+    const setNameState = useSessionStore(state => state.setNameState);
+    const resetState = useSessionStore(state => state.resetState);
+
+    setTimeout(() => {
+        if (session.state.name === "Login" && session.state.value === false) {
+            Alert.alert('', session.state.message);
+            resetState();
+        }
+
+        if (session.state.name === "Logout") {
+            Alert.alert('', session.state.message);
+            setNameState("Login");
+            resetState();
+        }
+        
+        if (session.state.name === "Signup" && session.state.value !== null) {
+            Alert.alert('', session.state.message);  
+            setNameState("Login");
+            resetState();
+        }
+    }, 1000);
+
     return (
         <>
             {
                 session.state.name === "Login" && session.state.value ?
                     <Profile session={session} />
                 : session.state.name === "Signup" ?
-                    <Signup session={session} />
+                    <Signup />
                 :
-                    <Login session={session} />
+                    <Login />
             }
         </>
     );
